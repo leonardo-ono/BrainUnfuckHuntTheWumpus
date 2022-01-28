64e2344c5b195b1f83841ef5681cd8c0f46ddcdc
Leonardo Ono <ono.leo80@gmail.com>
2022-01-28 20:27:24 -0300

    fixed infinite loop issue.

--- compileExec.bat
+++ compileExec.bat
@@ -1,4 +1,5 @@
 REM in notepad++, use the option Run > Run: $(CURRENT_DIRECTORY)\compileExec.bat $(NAME_PART) $(CURRENT_DIRECTORY)
+cd /d %2
 %2\BrainUnfuckCompiler %1.bu %1.bf
-java -jar %2\JavaBrainFuckInterpreter.jar %2\%1.bf
+java -jar JavaBrainFuckInterpreter.jar %1.bf
 
